9a4811325a32707ec953914f77a44ab5c1567d64
Aleksey Lymar <yoptar@gmail.com>
2018-03-29 12:38:01 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+let categories = ["analytics-automation", "ar", "cin", "ciscoit", "cle", "cloud", "collaboration", "csr", "customerstories",
+    "datacenter", "developer", "digital", "digital-joulex", "diversity", "education", "energy", "enterprise", "financialservices",
+    "getyourbuildon", "gov", "government", "healthcare", "innovation", "lifeatcisco", "manufacturing", "news", "openatcisco",
+    "partner", "performance", "perspectives", "retail", "security", "security-talos", "services", "smallbusiness", "socialmedia",
+    "sp", "video", "wireless", "zzfeatured"];
+
+categories = categories.map(name => ({name, open: false, files: null}));
+
+new Vue({
+    el: '#app',
+    data: {
+        categories,
+        selected: null
+    },
+    methods: {
+        open(category){
+            if(category.files !== null){
+                category.open = !category.open;
+                return;
+            }
+
+            this.$http.get(`map/${category.name}.json`).then(function(response){
+                category.files = response.body;
+                category.open = !category.open;
+            }, function(response){
+                console.log('ERROR!');
+            })
+        },
+        select(category, fileName){
+            this.selected = `${category.name}/${fileName}`
+        }
+    }
+})
